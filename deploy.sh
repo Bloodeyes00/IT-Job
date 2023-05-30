3e62bf8dea56d96b3a08013200ec758788aa1abe
Bloodeyes00 <zebs2211@gmail.com>
2023-05-30 23:05:50 +0500

    Responsiveness

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash 
 # Deployment script for GitHub 
 # Push all changes to GitHub 
-git remote add origin git@github.com:Bloodeyes00/IT-Job.git
+# git remote add origin git@github.com:Bloodeyes00/IT-Job.git
 git add .
 git commit -m "Responsiveness"
 git push -u origin shahrukh
