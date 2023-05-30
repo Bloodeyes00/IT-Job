c986b1cacbec635cf155b13a59f78a6577cbb487
Bloodeyes00 <zebs2211@gmail.com>
2023-05-31 04:47:04 +0500

    deployed to vercel

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # Push all changes to GitHub 
 git remote add origin https://github.com/Bloodeyes00/IT-Job.git
 git add .
-git commit -m "Icons Added"
+git commit -m "deployed to vercel"
 git push -u origin main
